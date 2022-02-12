9da5ad4c19f995fc0af26278a4a43763209d988c
Peter Vereshagin <peter@vereshagin.org>
2022-02-12 06:45:06 +0000

    test: :truck: rername according to 'autocode'

--- solution_test.go
+++ solution_test.go
@@ -6,7 +6,7 @@ import (
 
 const testText = "Hello \U0001f5fa\ufe0f!"
 
-func TestMessage(t *testing.T) {
+func TestGetMessage(t *testing.T) {
 	rendered := GetMessage()
 	if testText != rendered {
 		t.Error("GetMessage", rendered, testText)
